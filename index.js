93c2579409a70a0b6a7dce3f10a0e8e64248501a
ananya92 <1992ananya@gmail.com>
2020-02-05 23:25:42 +1030

    Added function to add new employee

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const inquirer = require(`inquirer`);
 
 const selectQuery = `SELECT e1.id AS Employee_ID, e1.first_name AS First_Name, e1.last_name AS Last_Name, r.title AS Role, d.name AS Department, r.salary as Salary, concat(e2.first_name,' ', e2.last_name) AS Manager FROM employee e1 INNER JOIN role r ON e1.role_id = r.id INNER JOIN department d ON r.department_id = d.id LEFT JOIN employee e2 ON e1.manager_id = e2.id`;
 
+const selManagersQuery = `SELECT concat(e2.first_name,' ', e2.last_name) as manager FROM employee e1 JOIN employee e2 ON e1.manager_id = e2.id`;
+
+const insertEmpQuery1 = `INSERT into employee(first_name, last_name, role_id) values(?,?,(SELECT id FROM role WHERE title = ?))`;
+const insertEmpQuery2 = `INSERT into employee(first_name, last_name, role_id, manager_id) values(?,?,(SELECT id FROM role WHERE title = ?),(SELECT id FROM employee WHERE concat(first_name,' ', last_name) = ?))`;
+
 require('events').EventEmitter.defaultMaxListeners = 25;
 //Creating MYSQL connection
 var connection = mysql.createConnection({
@@ -52,6 +57,7 @@ function start() {
                 viewEmployeesByManager();
                 break;
             case "Add new Employee, Role or department":
+                add();
                 break;
             case "Remove Employee, Role or department":
                 break;
@@ -115,11 +121,10 @@ function viewEmployeesByDepartment() {
 //Function to view information about employees under a given manager
 function viewEmployeesByManager() {
     //SQL query to select all manager names
-    connection.query("SELECT concat(e2.first_name,' ', e2.last_name) as manager FROM employee e1 JOIN employee e2 ON e1.manager_id = e2.id", function(err, result) {
+    connection.query(selManagersQuery, function(err, result) {
         if(err) throw err;
         var res=[];
         result.forEach(element => {
-            console.log(element.manager);
             res.push(element.manager);
         });
         //Give choice to user to select the manager
@@ -141,6 +146,94 @@ function viewEmployeesByManager() {
                     console.table(`\nPrinting all employees under ${data.choice}:`, res);
                 }
                 return start();
+            });
+        });
+    });
+}
+
+//Function to add new Employee, Role or Department
+function add() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What do you want to add?:",
+            choices: ["Employee", "Role", "Department"]
+        }
+    ]).then(function(data) {
+        switch(data.choice) {
+            case "Employee":
+                addEmployee();
+                break;
+            case "Role":
+                addRole();
+                break;
+            case "Department":
+                addDepartment();
+                break;
+        }
+    });
+}
+
+//Function to add new employee
+function addEmployee() {
+    //SQL query to fetch all employee roles
+    connection.query("SELECT title FROM role", function(err, result1) {
+        if(err) throw err;
+        //SQL query to fetch all Manager names
+        connection.query(selManagersQuery, function(err, result2) {
+            if(err) throw err;
+            var managers=[], roles=[];
+            result1.forEach(element => {
+                roles.push(element.title);
+            });
+            //Give option to not set Manager by providing option "None"
+            managers.push("None");
+            result2.forEach(element => {
+                managers.push(element.manager);
+            });
+            //Reading new employee's details
+            return inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Enter first name:",
+                    name: "first_name"
+                },
+                {
+                    type: "input",
+                    message: "Enter last name:",
+                    name: "last_name"
+                },
+                {
+                    type: "list",
+                    message: "Choose role:",
+                    name: "role",
+                    choices: roles
+                },
+                {
+                    type: "list",
+                    message: "Choose manager:",
+                    name: "manager",
+                    choices: managers
+                },
+            ]).then(function(data) {
+                var params = [];
+                var query = "";
+                //Creating the INSERT SQL query based on whether manager has been selected
+                if(data.manager == "None") {
+                    params = [data.first_name, data.last_name, data.role];
+                    query = insertEmpQuery1;
+                }
+                else {
+                    params = [data.first_name, data.last_name, data.role, data.manager]
+                    query = insertEmpQuery2;
+                }
+                //SQL INSERT query to add new employee
+                connection.query(query,params, function(err, result) {
+                    if(err) throw err;
+                    console.log(`\n ${result.affectedRows} Employee added successfully!\n`);
+                    start();
+                });
             });
         });
     });
